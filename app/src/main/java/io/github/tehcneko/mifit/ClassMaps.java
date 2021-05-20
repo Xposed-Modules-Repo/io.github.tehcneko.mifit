6a4c91484f1b9586d1083b5791a1be41bbcbde43
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-05-20 19:03:38 +0800

    Fix 5.1.0 (50433)

--- app/src/main/java/io/github/tehcneko/mifit/ClassMaps.java
+++ app/src/main/java/io/github/tehcneko/mifit/ClassMaps.java
@@ -6,10 +6,11 @@ import java.util.HashMap;
 
 public class ClassMaps {
     public static HashMap<Integer, Pair<String, String>> languageClassMap = new HashMap<>();
-    public static int maxSupportedVersion = 50429;
+    public static int maxSupportedVersion = 50433;
 
     static {
         languageClassMap.put(50426, new Pair<>("com.xiaomi.hm.health.o0000O00.OooOo", "OooOOoo"));
         languageClassMap.put(50429, new Pair<>("com.xiaomi.hm.health.o0000O00.OooOo", "OooOOoo"));
+        languageClassMap.put(50433, new Pair<>("com.xiaomi.hm.health.o0000O00.OooOo", "OooOOoo"));
     }
 }
