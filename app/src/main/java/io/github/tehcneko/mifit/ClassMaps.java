3642150f10b1d6b256a0cbb573286b583cc2dbbe
NekoInverter <chsqwyx@gmail.com>
2021-10-24 11:01:46 +0800

    Add support for 5.4.0 (50487)

--- app/src/main/java/io/github/tehcneko/mifit/ClassMaps.java
+++ app/src/main/java/io/github/tehcneko/mifit/ClassMaps.java
@@ -6,12 +6,13 @@ import java.util.HashMap;
 
 public class ClassMaps {
     public static HashMap<Integer, Pair<String, String>> languageClassMap = new HashMap<>();
-    public static int maxSupportedVersion = 50472;
+    public static int maxSupportedVersion = 50487;
 
     static {
         languageClassMap.put(50426, new Pair<>("com.xiaomi.hm.health.o0000O00.OooOo", "OooOOoo"));
         languageClassMap.put(50429, new Pair<>("com.xiaomi.hm.health.o0000O00.OooOo", "OooOOoo"));
         languageClassMap.put(50433, new Pair<>("com.xiaomi.hm.health.o0000O00.OooOo", "OooOOoo"));
         languageClassMap.put(50472, new Pair<>("com.xiaomi.hm.health.o0000O00.OooOo", "OooOo00"));
+        languageClassMap.put(50487, new Pair<>("com.xiaomi.hm.health.o0000oo.OooOo", "OooOo00"));
     }
 }
